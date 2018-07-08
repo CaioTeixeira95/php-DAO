c1834c5139bc96f12e20e70b7c0174a595a39ec6
Caio Teixeira <caio.xd63@hotmail.com>
2018-07-08 16:39:37 -0300

    Métodos de Lista

--- class/Usuario.php
+++ class/Usuario.php
@@ -60,6 +60,48 @@ class Usuario {
 
 	}
 
+	public static function getList(){
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM usuarios ORDER BY login");
+
+	}
+
+	public static function search($login){
+
+
+		$sql = new Sql();
+
+		return $sql->select("SELECT * FROM usuarios WHERE login LIKE :search ORDER BY login", array(
+			":search"=>"%".$login."%"
+		));
+	}
+
+	public function login($login, $senha){
+
+		$sql = new Sql();
+
+		$result = $sql->select("SELECT * FROM usuarios WHERE login = :login AND senha = :senha", array(
+			":login"=>$login,
+			":senha"=>$senha
+		));
+
+		//var_dump($result);
+
+		if(count($result) > 0){
+			$row = $result[0];
+
+			$this->setIdUsuario($row['idusuarios']);
+			$this->setLogin($row['login']);
+			$this->setSenha($row['senha']);
+			$this->setDataCadastro(new DateTime($row['data_cadastro']));
+		} else {
+			throw new Exception("Login e/ou senha inválidos.");		
+		}
+
+	}
+
 	public function __toString(){
 
 		return json_encode(array(
--- index.php
+++ index.php
@@ -2,10 +2,22 @@
 
 require_once("config.php");
 
-$user = new Usuario();
-
+// Carrega apenas um usuário
+/*$user = new Usuario();
 $user->loadById(2);
+echo $user;*/
 
-echo $user;
+//Carrega todos os usuários da tabela
+/*$lista = Usuario::getList();
+echo json_encode($lista);*/
+
+//Carrega o usuário buscando pelo login
+/*$busca = Usuario::search("us");
+echo json_encode($busca);*/
+
+//Carrega um usuário, usando o login e senha
+$usuario = new Usuario();
+$usuario->login("usuario", "123465");
+echo $usuario;
 
  ?>
